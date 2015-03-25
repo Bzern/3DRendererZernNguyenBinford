a46844dd517c94cb1c90c65b3dd1980cde41657c
Bzern <benzern@gmail.com>
2015-03-25 11:12:20 -0500

    Cleaned GameObject
    get/set

---
+++ src/New/GameObject.java
@@ -0,0 +1,20 @@
+public class GameObject {
+	Polygon3D[] faces;
+
+	public GameObject(Polygon3D[] a) {
+		faces = a;
+	}
+
+	public void act() {
+		for (Polygon3D p : faces) {
+			for (Point3D a : p.getPolygon3Dpoints()) {
+				Point3D c = a.rotate(faces[5].center(), new Point3D(
+						0, 0.01, 0.01));
+				a.setX(c.getX());
+				a.setY(c.getY());
+				a.setZ(c.getZ());
+			}
+		}
+	}
+
+}
