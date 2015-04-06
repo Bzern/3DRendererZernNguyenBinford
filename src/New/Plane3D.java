5f7e1fe7b7303c81c5d29bc32a62175b10859ef7
wilsonnguyen <wilsondon07@gmail.com>
2015-04-06 11:15:22 -0500

    aded

--- src/New/Plane3D.java
+++ src/New/Plane3D.java
@@ -10,7 +10,8 @@ public class Plane3D {
       a = prod.getX();
       b = prod.getY();
       c = prod.getZ();
-      k = (-1*p2.getX())+(-1*p2.getY())+(-1*p2.getZ());
+      k = (-1*p1.getX())+(-1*p1.getY())+(-1*p1.getZ());
+      System.out.println("this is k"+k);
    }
    public Point3D crossprod(Point3D a,Point3D b){
       Point3D cross = new Point3D(a.getY()*b.getZ()-a.getZ()*b.getY(),a.getZ()*b.getX()-a.getX()*b.getZ(),
@@ -30,6 +31,12 @@ public class Plane3D {
       return k;
    }
    public double getZ(Point2D bob){
+      System.out.println(bob.getX()+ "sdgsfsffsf");
+      System.out.println(a);
+      System.out.println(bob.getY());
+      System.out.println(b);
+      System.out.println(k);
+      System.out.println(c);
       return (bob.getX()*a*-1 + bob.getY()*b*-1-k)/c;
    }
    
@@ -40,7 +47,7 @@ public class Plane3D {
       Point3D p3 = new Point3D(9,7,2);
       System.out.println(p1+"\n"+p2+"\n"+p3);
       Plane3D bob = new Plane3D(p1, p2, p3);
-      System.out.println(bob.getA() +" "bob.getB()+" "+bob.getC()+bob.getZ(new Point2D(3,6)));
+      System.out.println(bob.getA() +" "+bob.getB()+" "+bob.getC()+" "+bob.getK()+ " "+bob.getZ(new Point2D(3,6)));
    
    }
 }
