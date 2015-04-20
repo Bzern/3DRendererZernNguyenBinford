1442f637b07973bb486b4bf4c60814f14579284c
Bzern <benzern@gmail.com>
2015-04-20 10:31:01 -0500

    Matrix works

--- src/New/Matrix.java
+++ src/New/Matrix.java
@@ -4,12 +4,23 @@ public class Matrix{
    public Matrix(double[][] mat){
       matrix = mat;
    }
-
+ /* 
+  * public static void main(String[] args)
+  * {
+  *    double arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+  *    double arr2[][] = {{9,8,7},{6,5,4},{3,2,1}};
+  *    Matrix mat = new Matrix(arr);
+  *    Matrix mat2 = new Matrix(arr2);
+  *    System.out.println(mat+"\n");
+  *    System.out.println(mat2+"\n");
+  *    System.out.println(mat.multiply(mat2)+"\n");
+  * }
+  */ 
    public double[][] getMatrix(){
       return matrix;
    }
 
-   public double[][] multiply(Matrix mat){
+   public Matrix multiply(Matrix mat){
       double [][] matrix1 = mat.getMatrix();
       double [][] ret = new double[matrix.length][matrix1[0].length];
       for (int i = 0; i < matrix.length; i++) { 
@@ -19,10 +30,10 @@ public class Matrix{
             }
          }
       }
-      return ret;
+      return new Matrix(ret);
    }
    
-   public double[][] multiply(double[][] matrix1){
+   public Matrix multiply(double[][] matrix1){
       double [][] ret = new double[matrix.length][matrix1[0].length];
       for (int i = 0; i < matrix.length; i++) { 
          for (int j = 0; j < matrix1[0].length; j++) { 
@@ -31,10 +42,23 @@ public class Matrix{
             }
          }
       }
-      return ret;
+      return new Matrix(ret);
    }
    
    public String toString(){
-      return matrix.toString();
+      String ret = "";
+      for(int r = 0; r < matrix.length; r++)
+      {
+         if(r>0)
+            ret+= "\n";
+         ret+="[";
+         for(int c = 0; c < matrix[r].length; c++)
+         {
+            ret += (" "+matrix[r][c]);
+         }
+         ret+= " ]";
+      }
+
+      return ret;
    }
 }
